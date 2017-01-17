ab30634200a9c94a42f82096897f8e4fefedba88
Irene Canuti <irene.canuti@gmail.com>
2017-01-17 16:39:58 +0000

    Testing with Capybara. Getting error 'rack-test requires a rack application, but none was given'

--- Gemfile
+++ Gemfile
@@ -5,6 +5,10 @@ ruby '2.3.0'
 
 # gem "rails"
 gem 'sinatra'
+gem 'rspec'
 gem 'rspec-sinatra'
 gem 'capybara'
 gem 'pry'
+gem 'rails-helper'
+gem 'selenium-webdriver'
+gem 'shotgun'
--- lib/app.rb
+++ lib/app.rb
@@ -1,9 +1,18 @@
-require 'sinatra'
-require 'rspec-sinatra'
+require 'sinatra/base'
 
-class Battle < Sinatra::Base
-  set :sessions, true
+class Battle < Sinatra::Application
 
   get '/' do
-    erb(:index)
+    erb :index
   end
+
+  post '/names' do
+    p params
+    @name = params[:player1]
+    @name = params[:player2]
+    erb :play
+  end
+
+  # start the server if ruby file executed directly
+  run! if app_file == $0
+end
